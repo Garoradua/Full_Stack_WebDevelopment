9d651c909eb8d44b77a2e4ca5d937eeea6f49f61
Gourav Arora <gouravarora035@gmail.com>
2021-06-03 18:07:00 +0530

    js

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -8,14 +8,28 @@
 //  console.log('c' - 5)
 
 
-var no = 100;
-console.log(typeof(no.toString()));
- console.log(parseInt('44'))
- console.log(parseInt('22.222'))
- console.log(parseInt('Banana'))
- console.log(parseFloat(12))
- console.log(parseFloat(13.3))
- var num1 = 10; console.log(num1.toFixed(2))
- var num1 = 15.456789; console.log(num1.toFixed())
- var num1 = 15.456789; console.log(num1.toFixed(2))
- var num1 = 15.456789; console.log(num1.toFixed(3))
+// var no = 100;
+// console.log(typeof(no.toString()));
+//  console.log(parseInt('44'))
+//  console.log(parseInt('22.222'))
+//  console.log(parseInt('Banana'))
+//  console.log(parseFloat(12))
+//  console.log(parseFloat(13.3))
+//  var num1 = 10; console.log(num1.toFixed(2))
+//  var num1 = 15.456789; console.log(num1.toFixed())
+//  var num1 = 15.456789; console.log(num1.toFixed(2))
+//  var num1 = 15.456789; console.log(num1.toFixed(3))
+
+
+
+// var mNum;
+// console.log(mNum)
+// =================
+// var mNum = null
+// console.log(mNum)
+// 1. What will be the result of first and second console.log().
+
+// console.log(undefined == null)
+// console.log(undefined === null)
+// var num = window.confirm();
+// console.log(num);
