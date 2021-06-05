99d452e18d3cc8c2fcdcb13920e1f2fbd245d3cb
Gourav Arora <gouravarora035@gmail.com>
2021-06-05 16:32:52 +0530

    js

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -34,15 +34,107 @@
 // var num = window.confirm();
 // console.log(num);
 
-var n = 2;
-var p = 4;
-var q = 5;
-var w = 3;
-if ( !((p * q) /n <= (q * w) + n/p ))
-{
-	console.log( ++p + w++ + " " + ++n);
-}
-else
-{
-	console.log(--p + q-- + " " + --n);
+// var n = 2;
+// var p = 4;
+// var q = 5;
+// var w = 3;
+// if ( !((p * q) /n <= (q * w) + n/p ))
+// {
+// 	console.log( ++p + w++ + " " + ++n);
+// }
+// else
+// {
+// 	console.log(--p + q-- + " " + --n);
+// }
+
+// Let's see some more example for conversion to string types
+// console.log(typeof('hola' + true))
+// console.log(typeof(null + 'hola' ))
+// console.log(typeof(5 + 'hola'))
+
+// Let's see some example for conversion to integer types
+// console.log(typeof(+'2'))
+// console.log(typeof(-'10'))
+// console.log(typeof(+'2'))
+// console.log(typeof(2 >= '2'))
+
+// What will be printed in the console.
+// console.log(true + false)
+// console.log(12 / "6")
+// console.log("number" + 15 + 3)
+// console.log(15 + 3 + "number")
+// console.log(1 > null)
+// console.log("foo" + + "bar")
+// console.log('true' == true)
+// console.log(false == 'false')
+// console.log(null == '')
+// console.log(!!"false" == !!"true")
+
+
+// console.log(typeof(String(true)))
+// console.log(typeof(String(-98.13)))
+// console.log(typeof(String(null)))
+// console.log(typeof(String(undefined)))
+
+// // Similarly, to convert a value to number datatype, we need to use the inbuilt Number() function. Let's try some examples.
+// console.log(typeof(Number('2')))
+// console.log(typeof(Number('-20.198')))
+// console.log(typeof(Number('apple')))
+// console.log(typeof(Number(true)))
+// console.log(typeof(Number(false)))
+
+// var mCars = {
+//     p1: "350 kmph",
+//     gallardo: "320 kmph",
+//     veyron: "409 kmph",
+//     agera: "429 kmph"
+// }
+// console.log(mCars)
+// console.log(typeof(mCars))
+
+
+// mArr=[9,5,4,3,7,1,0]
+// console.log(mArr.sort(compare))
+// function compare(num1,num2){
+//     // if(num1<num2) return -1;
+//     if(num1>num2){
+//          return 1;
+//     }
+//     else {
+//         return 0;
+//     }
+
+// }
+var name;
+
+var age = null;
+
+if(name) {
+
+  console.log(4)
+
+  if (0) {
+
+   console.log(5)
+
+  } else {
+
+   console.log(6)
+
+ }
+
+} else {
+
+console.log(1)
+
+ if (age) {
+
+   console.log(2)
+
+  } else {
+
+   console.log(3)
+
+ }
+
 }
