c9a0bb80485e8e87fdb637ece754e3372cdd718e
Gourav Arora <gouravarora035@gmail.com>
2021-06-17 11:54:47 +0530

    card with local storage

--- Card with delete functionality/index.js
+++ Card with delete functionality/index.js
@@ -7,32 +7,27 @@ var inputBox = document.getElementById("input-box");
 
 var mainCard = document.getElementById("main-card");
 var cDate = new Date();
-inputBox.addEventListener("keypress", function(e){
 
-if(e.key=="Enter"){
-    // <!-- <div id="upper-div">
-    //     <div class="inner-div" >
-    //         <h3> Buy Apples </h3>
-    //         <p> Date </p>
-    //     </div>
-    //     <i class="fas fa-trash-alt"></i>
-    // </div> -->
-    if(inputBox.value == ""){
-        alert("Null Item");
-        return;
-    }
-    var upperDiv = document.createElement("div");
+var fromLocalStorage = localStorage.getItem("todo-list") == null ? [] : JSON.parse(localStorage.getItem("todo-list"));
+for(i=0; i<fromLocalStorage.length; i++){
+    TODOrender(fromLocalStorage[i].message, fromLocalStorage[i].time);
+}
+
+
+
+function TODOrender(message, date){
+var upperDiv = document.createElement("div");
     upperDiv.id = "upper-div";
 
     var innerDiv = document.createElement("div");
     innerDiv.className = "inner-div";
 
     var heading = document.createElement("h3");
-    heading.innerHTML = inputBox.value;
+    heading.innerHTML = message;
 
     var paragraph = document.createElement("p")
     var cDate = new Date();
-    paragraph.innerHTML = cDate.getDate() + "-" + (parseInt(cDate.getMonth()) + 1) + "-" +cDate.getFullYear();
+    paragraph.innerHTML = date;
     var icon = document.createElement("i");
     icon.className = "fas fa-trash-alt";
 
@@ -49,7 +44,48 @@ icon.onclick = function(){
     upperDiv.remove();
 }
 }
-      
+
+
+
+
+// var mArr={
+//     key: heading.innerHTML
+// }
+
+// localStorage.setItem("input-value", JSON.stringify(mArr));
+// var storedItem = localStorage.getItem("input-value");
+// console.log(JSON.parse(storedItem)); 
+
+
+
+
+
+inputBox.addEventListener("keypress", function(e){
+if(e.key=="Enter"){
+    // <!-- <div id="upper-div">
+    //     <div class="inner-div" >
+    //         <h3> Buy Apples </h3>
+    //         <p> Date </p>
+    //     </div>
+    //     <i class="fas fa-trash-alt"></i>
+    // </div> -->
+    if(inputBox.value == ""){
+        alert("Null Item");
+        return;
+    }
+    var date = cDate.getDate() + "-" + (parseInt(cDate.getMonth()) + 1) + "-" +cDate.getFullYear();
+    var mArr = localStorage.getItem("todo-list") == null ? [] : JSON.parse(localStorage.getItem("todo-list"));
+    mArr.push({
+    message: inputBox.value,
+    time: date
+    
+        })
+
+localStorage.setItem("todo-list", JSON.stringify(mArr));
+
+TODOrender(inputBox.value, date);  
+}
+  
 
 })
 
@@ -58,4 +94,5 @@ icon.onclick = function(){
 //         if(e.key == "Enter"){
 //         console.log(e);
 //         }
-// })
+// }
+
