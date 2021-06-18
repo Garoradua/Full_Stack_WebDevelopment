2239a5e9abe9cfd3f90fc2deace89254663cf86b
Gourav Arora <gouravarora035@gmail.com>
2021-06-18 09:36:27 +0530

    HTTP 1

---
+++ video-grid/index.js
@@ -0,0 +1,17 @@
+console.log("script");
+var videoGrid = document.getElementById("videogrid");
+var connection = new XMLHttpRequest();
+connection.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist", true);
+connection.send();
+connection.onreadystatechange = function(){
+    if(this.readyState == 4){
+        var response = JSON.parse(this.responseText);
+        if(response.length>0){
+            for(i=0; i<response.length; i++){
+                var title = document.createElement("img");
+                title.src = response[i].thumbnail;
+                videoGrid.appendChild(title);
+            }
+        }
+    }
+}
