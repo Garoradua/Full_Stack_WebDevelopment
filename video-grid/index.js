a3388ab97455a878d841366aeb54ec0f8c58b66f
Gourav Arora <gouravarora035@gmail.com>
2021-06-18 13:20:22 +0530

    http 1

--- video-grid/index.js
+++ video-grid/index.js
@@ -1,11 +1,15 @@
 console.log("script");
 var videoGrid = document.getElementById("videogrid");
+
+
 var connection = new XMLHttpRequest();
 connection.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist", true);
 connection.send();
+
 connection.onreadystatechange = function(){
-    if(this.readyState == 4){
+    if(connection.readyState == 4){
         var response = JSON.parse(this.responseText);
+        console.log(this);
         if(response.length>0){
             for(i=0; i<response.length; i++){
                 var title = document.createElement("img");
@@ -15,3 +19,5 @@ connection.onreadystatechange = function(){
         }
     }
 }
+
+// console.log(this);
