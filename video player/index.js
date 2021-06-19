5d6e09d894025719083412594090c7ae48fcc341
Gourav Arora <gouravarora035@gmail.com>
2021-06-19 23:39:32 +0530

    video player

---
+++ video player/index.js
@@ -0,0 +1,33 @@
+/* <div id="card1" class="playlist-card active-card">
+<img class="thumbnail" src="https://i.vimeocdn.com/video/600595198_390x220.webp">
+<h3>Croissants | Flour and Stone</h3>
+</div> --> */
+
+var playlistWrapper = document.getElementById("playlist-wrapper");
+var videoPlayer = document.getElementById("video-player");
+videoPlayer.src = "https://player.vimeo.com/video/190062231";
+var http = new XMLHttpRequest;
+http.open("GET", "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist", true);
+http.onreadystatechange = function(){
+    if(http.readyState==4){
+    var response = JSON.parse(this.responseText);
+    // console.log(response);
+    for(var i=0; i<response.length; i++){
+ var playlistCard = document.createElement("div");
+ playlistCard.className = "playlist-card";
+ var pic = document.createElement("img");
+ pic.className = "thumbnail";
+ pic.src = response[i].thumbnail;
+ var heading = document.createElement("h3");
+ heading.innerHTML = response[i].title;
+ playlistCard.appendChild(pic);
+ playlistCard.appendChild(heading);
+playlistWrapper.appendChild(playlistCard);
+}
+// playlistWrapper.addEventListener("click".function{
+//     videoPlayer.src
+// })
+    }
+}
+// https://5d76bf96515d1a0014085cf9.mockapi.io/video/
+http.send()
